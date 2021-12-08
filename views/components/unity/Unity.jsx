a4b8e19dc48f719f4340361a4e29369309fb6662
TyperGuy <ray.developper@gmail.com>
2021-12-08 14:00:43 +0100

    feat: unity component

--- views/components/unity/Unity.jsx
+++ views/components/unity/Unity.jsx
@@ -9,26 +9,23 @@ function Unity(){
   const local = 'Luanda, Talatona, Via A12';
   return (
     <div className={Styles.unityContainer}>
-      <img className={Styles.unityIMG} src={img} alt=""/>
-      <div className="row"><p className="unity-name">SIAC-Kalawenda</p><span className="unity-estado">Aberto</span></div>
-      <hr className="unity-simple-line"/>
-      <div className="row">
-        <li ><i className="fas fa-clock"/> <span>Abre: {entrada}</span></li>
-        <li ><i className="fas fa-clock"/> <span>Fecha: {saida}</span></li>
-      </div>
-      <p className="unity-descricao">
+      <div className={Styles.unityChildContainer}>
+        <img className={Styles.unityIMG} src={img} alt=""/>
+        <div className={Styles.row}><p className={Styles.unityName}>SIAC-Kalawenda</p><span className="unity-estado">Aberto</span></div>
+        <hr className={Styles.unitySimpleLine}/>
+        <div className={Styles.row}>
+           <li ><i className="fas fa-clock"/> <span>Abre: {entrada}</span></li>
+           <li ><i className="fas fa-clock"/> <span>Fecha: {saida}</span></li>
+        </div>
+        <p className="unity-descricao">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
          Porro totam reiciendis deleniti dignissi...
-      </p>
-
-      <div className="row location">
-        <p><i className="fas fa-map-marker-alt"/> <span>{local}</span></p>
-      </div>
-
-     <button className="locate-btn">Leva-me lá</button>
-      
- 
-
+        </p>
+        <div className="row location">
+          <p><i className="fas fa-map-marker-alt"/> <span>{local}</span></p>
+        </div>
+        <button className="locate-btn">Leva-me lá</button>
+      </div>   
     </div>
   );
     
