a859faf757fb72116986e9f0e16316da583a3460
TyperGuy <ray.developper@gmail.com>
2021-12-08 15:53:01 +0100

    feat : unity Component

--- views/unities/Unities.jsx
+++ views/unities/Unities.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Unity from '../components/unity/Unity';
+import styled from 'styled-components';
+import Styles from './Unities.module.css';
 
 export default function Unities(){
+  const Container = styled.div` 
+    width:100vw;
+    background:linear-gradient(90deg,#1E195A,#1679B6,#194A68);
+  `
    return(
-     <div>
-       <Unity/>
-     </div>
+     <Container>
+       <div className={Styles.slideContainer}>
+        <Unity/>
+       </div>
+     </Container>
    )
 }
