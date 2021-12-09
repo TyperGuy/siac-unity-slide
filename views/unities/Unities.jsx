56696d4873ec6f56e629fd8a054990e68df87b6d
TyperGuy <ray.developper@gmail.com>
2021-12-09 16:31:03 +0100

    feat : Unity List

--- views/unities/Unities.jsx
+++ views/unities/Unities.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Unity from '../components/unity/Unity';
 import styled from 'styled-components';
 import Styles from './Unities.module.css';
+import {MdChevronLeft as Mdleft,MdChevronRight as Mdright} from 'react-icons/md';
 
 export default function Unities(){
   const Container = styled.div` 
@@ -10,10 +11,29 @@ export default function Unities(){
     display:flex;
     justify-content:center;
     `
+  const Circle = styled.div`
+  margin: 10px ;
+  display: flex;
+  align-items: center;
+  align-content: center;
+  box-shadow: 0 5px 15px rgba(109, 121, 145, 0.9);
+  border-radius: 50%;
+  background-color: transparent;
+  width: 30px;
+  height: 30px;
+  transition: transform 0.3s ease-in-out;
+  font-size:2.2rem;
+  color: #1679B6;
+  background-color: #fff;
+  &:hover{
+    transform:scale(1.2);
+    color: #F52724;
+  }
+  `  
    return(
      <Container>
        <div className={Styles.containerChild}>
-          <h1>Proximo</h1>
+          <Circle><Mdleft/></Circle>
           <div className={Styles.slideContainer}>
             <Unity/>
             <Unity/>
@@ -25,7 +45,7 @@ export default function Unities(){
             <Unity/>
             <Unity/>
           </div>
-          <h1>Anterior</h1>
+          <Circle><Mdright/></Circle>
        </div>
       
      </Container>
