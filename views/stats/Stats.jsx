e3e4791d9831e28c9cfae2bb6b0494c3e81a2f84
TyperGuy <ray.developper@gmail.com>
2021-12-17 12:12:26 +0100

    feat: Statistics component

--- views/stats/Stats.jsx
+++ views/stats/Stats.jsx
@@ -1,12 +1,13 @@
 
 import Styles from './Stats.module.css';
+import StatsFile from './Stats.json';
 
 const Stats = (props) =>{
 
   return(
     <div className={Styles.statsContainer}>
       <div className={Styles.statsInnerContainer}>
-        Este é o Component de Status
+        <div className={Styles.titleContainer}><h2>{StatsFile.title}</h2></div>
       </div>
     </div>
   )
