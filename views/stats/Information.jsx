62ca5935cbfdf85191ec7f4a35fac8bb830bf3c5
TyperGuy <ray.developper@gmail.com>
2021-12-17 14:33:07 +0100

    chore: Instaling react-icons dependency

---
+++ views/stats/Information.jsx
@@ -0,0 +1,8 @@
+import Styles from './Stats.module.css';
+const Information = (props) =>{
+  return(
+    <div className={Styles.statsComponent}></div>
+  )
+}
+
+export default Information;
--- views/stats/Stats.jsx
+++ views/stats/Stats.jsx
@@ -1,6 +1,7 @@
 
 import Styles from './Stats.module.css';
 import StatsFile from './Stats.json';
+import Information from './Information.jsx';
 
 const Stats = (props) =>{
 
@@ -8,6 +9,9 @@ const Stats = (props) =>{
     <div className={Styles.statsContainer}>
       <div className={Styles.statsInnerContainer}>
         <div className={Styles.titleContainer}><h2>{StatsFile.title}</h2></div>
+        <div className={Styles.statsInfContainer}>
+          <Information/>
+        </div>
       </div>
     </div>
   )
