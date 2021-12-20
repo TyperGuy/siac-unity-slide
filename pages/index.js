df7c8402c3af9f12837fb409bfb8dddbb340162f
TyperGuy <ray.developper@gmail.com>
2021-12-20 13:36:15 +0100

    feat: partners components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import styles from '../styles/Home.module.css'
 import Unities from '../views/unities/Unities.jsx'
 import Stats from '../views/stats/Stats.jsx';
+import Partners from '../views/partners/Partners';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Stats/>
-      <Unities/>
+      <Partners/>
     </div>
   )
 }
---
+++ views/partners/Partners.jsx
@@ -0,0 +1,8 @@
+import Styles from './Partners.module.css';
+const Partners =()=>{
+  return(
+    <div className={Styles.partnersContainer}>Partener</div>
+  )
+}
+
+export default Partners;
