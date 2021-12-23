2dc5c722cf356983bba86beaac52a30ad2a62bae
TyperGuy <ray.developper@gmail.com>
2021-12-23 15:14:35 +0100

    feat: add image header to partners component

--- views/partners/Partners.jsx
+++ views/partners/Partners.jsx
@@ -1,27 +1,17 @@
 import Styles from './Partners.module.css';
 import Partner from './Partner.jsx';
 import IMgs from './parceiros/Exporter';
-import img from './parceiros/family.webp';
 const Partners =()=>{
   return(
-    <div>
-      
-      <div className={Styles.upContainer}>
-        <img className={Styles.img} src={img} alt="" />
-        <div className={Styles.contentContainer}></div>
-      </div>
-      <div className={Styles.partnersContainer}>
-        {
-          IMgs.map((partner)=>{
-            return(
-            <Partner img={partner}/>
-            )
-          })
-        }
-      </div>
-
+    <div className={Styles.partnersContainer}>
+     {
+      IMgs.map((partner)=>{
+        return(
+          <Partner img={partner}/>
+        )
+      })
+    }
     </div>
-   
   )
 }
 
